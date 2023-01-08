81e9b03a766560595844c61bb2decb56b7fdcc84
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-09 00:43:33 +0530

    commit :+1:

---
+++ index.js
@@ -0,0 +1,5 @@
+const Bill = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+
+checkButton.addEventListener("click", ()=> console.log("clicked"));
